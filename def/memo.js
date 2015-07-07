60305b9f4698ce76cca05a1eacaac6d996d8a162
chinakids <494063570@qq.com>
2015-07-07 18:23:55 +0800

    格式调整

--- def/memo.js
+++ def/memo.js
@@ -2,16 +2,22 @@
  * Generate ContextMenu Template
  */
 var sels = {};
-atom.contextMenu.itemSets.map(function(d){
-  if (!sels[d.selector]) { sels[d.selector] = [] }
+
+atom.contextMenu.itemSets.map(function(d) {
+  if (!sels[d.selector]) {
+    sels[d.selector] = []
+  }
   sels[d.selector] = sels[d.selector].concat(d.items);
 })
+
 var data = "";
+
 for (sel in sels) {
   data += '  "' + sel + '":\n'
-  sels[sel].map(function(d){
+  sels[sel].map(function(d) {
     if (d.type === "separator") return false;
-    data += '    "'+d.command+'": "'+d.label+'"\n'
+    data += '    "' + d.command + '": "' + d.label + '"\n'
   })
 }
+
 atom.clipboard.write(data);
